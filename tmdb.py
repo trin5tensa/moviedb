060262d562c801164ece8ef3d64975a41f071b99
Stephen Rigden <stepcomfive@wowway.com>
2020-12-27 07:47:21 -0500

    moviedb-225 Create Module tmdb.py
    Initial set up ready to start design and coding.

---
+++ tmdb.py
@@ -0,0 +1,55 @@
+"""Title of module (not the <name>.py).
+
+Description of module.
+"""
+
+#  Copyright ©2020. Stephen Rigden.
+#  Last modified 12/27/20, 7:47 AM by stephen.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# TODO Update module title and description
+# TODO Remove layout reminder comments.
+# Python package imports
+
+import sys
+
+
+# Third party package imports
+
+
+# Project Imports
+
+
+# Constants
+
+
+# API Classes, DataClasses, and TypedDicts
+
+
+# API Functions
+
+
+# Internal Module Classes, DataClasses, and TypedDicts
+
+
+# Internal Module Functions
+
+
+def main():
+    """Integration tests and usage examples."""
+    # TODO Remove this function or write integration tests
+    pass
+
+
+if __name__ == '__main__':
+    # TODO Remove this function or write integration tests
+    sys.exit(main())
