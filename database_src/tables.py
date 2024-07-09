0de30a09138523f551ad012939d0aac08deefee2
Stephen Rigden <stepcomsix@iCloud.com>
2024-07-09 14:00:37 -0400

    moviedb-385 moviedb Tag Support Functions
    Code and unit tests complete.

--- database_src/tables.py
+++ database_src/tables.py
@@ -1,7 +1,7 @@
 """Database table functions."""
 
 #  Copyright© 2024. Stephen Rigden.
-#  Last modified 7/9/24, 1:13 PM by stephen.
+#  Last modified 7/9/24, 2:00 PM by stephen.
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -36,9 +36,42 @@ def _select_all_tags(session: Session) -> set[schema.Tag]:
     """Returns a list of every tag.
 
     Args:
-        session: The current session
+        session:
     Returns:
         A set of tags.
     """
     statement = select(schema.Tag)
     return {tag for tag in session.scalars(statement).all()}
+
+
+def _add_tags(session: Session, tag_texts: list[str]):
+    """Adds new tags from a list of tag texts.
+
+    Args:
+        session:
+        tag_texts:
+    """
+    session.add_all([schema.Tag(text=tag) for tag in tag_texts])
+
+
+def _edit_tag(session: Session, old_tag_text: str, new_tag_text: str):
+    """Edits the text of a tag.
+
+    Args:
+        session:
+        old_tag_text:
+        new_tag_text:
+    """
+    tag = _select_tag(session, old_tag_text)
+    tag.text = new_tag_text
+
+
+def _delete_tag(session: Session, tag_text: str):
+    """Deletes a tag.
+
+    Args:
+        session:
+        tag_text:
+    """
+    tag = _select_tag(session, tag_text)
+    session.delete(tag)
