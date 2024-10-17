6ad0c098e127029d73b1fecec4236ae013e7b78a
Stephen Rigden <stepcomsix@iCloud.com>
2024-10-17 11:11:02 -0400

    moviedb-424 moviedb handlers._search_movie_callback
    Updates to moviebagfacade

--- gui_handlers/moviebagfacade.py
+++ gui_handlers/moviebagfacade.py
@@ -1,7 +1,7 @@
 """MovieBag Facade."""
 
 #  Copyright© 2024. Stephen Rigden.
-#  Last modified 10/5/24, 4:20 PM by stephen.
+#  Last modified 10/17/24, 11:10 AM by stephen.
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -15,15 +15,16 @@
 
 import copy
 
-from config import MovieKeyTypedDict, FindMovieTypedDict
+from config import MovieKeyTypedDict, FindMovieTypedDict, MovieUpdateDef
 from globalconstants import *
 
 
 class MovieBagFacade(MovieBag):
     """This subclass of MovieBag provides movie bag constructors.
 
-    It is temporary until the GUI API is rewritten for movie bags at which
-    time this subclass will become obsolete.
+    Use Case:
+        It is temporary until the GUI API is rewritten for movie bags at
+        which time this subclass will become obsolete.
     """
 
     # noinspection PyTypedDict
@@ -31,12 +32,16 @@ class MovieBagFacade(MovieBag):
     def from_movie_key_typed_dict(cls, movie: MovieKeyTypedDict) -> "MovieBagFacade":
         """Converts a MovieKeyTypedDict into a MovieBag object.
 
-        Args:
-        print(f"{movie=}")
-            movie:
+            Args:
+            print(f"{movie=}")
+                movie:
 
-        Returns:
-            A movie bag object.
+            Returns:
+                A movie bag object.
+
+        Use Case:
+            It is temporary until the GUI API is rewritten for movie bags at
+            which time this method will become obsolete.
         """
         return cls(title=movie["title"], year=MovieInteger(movie["year"]))
 
@@ -50,6 +55,10 @@ class MovieBagFacade(MovieBag):
 
         Returns:
             A movie bag object.
+
+        Use Case:
+            It is temporary until the GUI API is rewritten for movie bags at
+            which time this method will become obsolete.
         """
         movie = copy.deepcopy(movie)  # Testing support (before and after)
         # noinspection PyTypeChecker
@@ -76,23 +85,86 @@ class MovieBagFacade(MovieBag):
 
         Returns:
             A movie bag object.
+
+        Use Case:
+            It is temporary until the GUI API is rewritten for movie bags at
+            which time this method will become obsolete.
         """
-        movie = copy.deepcopy(movie)  # Testing support (before and after)
+        movie_bag = cls.__new__(MovieBagFacade)
+        if movie.get("title"):
+            movie_bag["title"] = movie["title"]
         if movie.get("year"):
-            year_range = f"{movie['year'][0]}-{movie['year'][1]}"
-            # noinspection PyTypeChecker
-            movie["year"] = MovieInteger(year_range)
+            movie_bag["year"] = _range_converter(movie["year"])
         if movie.get("director"):
-            # noinspection PyTypeChecker
-            movie["directors"] = {movie["director"]}
-            del movie["director"]
+            movie_bag["directors"] = set(movie["director"].split(", "))
         if movie.get("minutes"):
-            duration_range = f"{movie['minutes'][0]}-{movie['minutes'][1]}"
-            # noinspection PyTypeChecker
-            movie["duration"] = MovieInteger(duration_range)
-            del movie["minutes"]
+            movie_bag["duration"] = _range_converter(movie["minutes"])
+        if movie.get("notes"):
+            movie_bag["notes"] = movie["notes"]
         if movie.get("tags"):
-            # noinspection PyTypeChecker
-            movie["movie_tags"] = {movie for movie in movie["tags"]}
-            del movie["tags"]
-        return cls(movie)
+            movie_bag["movie_tags"] = {movie for movie in movie["tags"]}
+        return movie_bag
+
+
+def convert_to_movie_key(movie_bag: MovieBag) -> MovieKeyTypedDict:
+    """Returns a config.MovieKeyTypedDict object.
+
+    Use Case:
+        It is temporary until the GUI API is rewritten for movie bags at
+        which time this function will become obsolete.
+    """
+    movie_key = MovieKeyTypedDict(
+        title=movie_bag["title"],
+        year=int(movie_bag["year"]),
+    )
+    return movie_key
+
+
+def convert_to_movie_update_def(movie_bag: MovieBag) -> MovieUpdateDef:
+    """Converts a new style MovieBag object into an old style MovieUpdateDef object.
+
+    Args:
+        movie_bag:
+
+    Returns:
+        A MovieUpdateDef object
+
+    Use Case:
+        It is temporary until the GUI API is rewritten for movie bags at
+        which time this function will become obsolete.
+    """
+    movie = MovieUpdateDef(**convert_to_movie_key(movie_bag))
+    if movie_bag.get("directors"):
+        movie["director"] = list(movie_bag.get("directors"))
+    if movie_bag.get("duration"):
+        movie["minutes"] = int(movie_bag.get("duration"))
+    if movie_bag.get("notes"):
+        movie["notes"] = movie_bag.get("notes")
+    if movie_bag.get("movie_tags"):
+        movie["tags"] = list(movie_bag.get("movie_tags"))
+    return movie
+
+
+def _range_converter(value: Sequence[str]) -> MovieInteger:
+    """Converts a 'range' string into a MovieInteger object.
+
+    Args:
+        value: The year and minutes item in the old style FindMovieTypedDict
+        contained a sequence of stings. The intended use was for either
+        a sing numeric string or a pair of numeric strings.
+        For example: [1960] or [1960, 1965].
+
+    Returns:
+        A MovieInt object.
+    """
+    # not todo Common code already tested within caller
+    match len(value):
+        case 1:
+            duration_range = f"{value[0]}"
+        case 2:
+            duration_range = f"{value[0]}-{value[1]}"
+        case _:
+            raise ValueError(
+                f"Length of value must be 1 or 2 not" f" {len(value)}. {value=}"
+            )
+    return MovieInteger(duration_range)
--- test/gui_handlers/test_moviebagfacade.py
+++ test/gui_handlers/test_moviebagfacade.py
@@ -6,7 +6,7 @@ the DBv1 database.
 """
 
 #  Copyright© 2024. Stephen Rigden.
-#  Last modified 9/26/24, 6:30 AM by stephen.
+#  Last modified 10/17/24, 11:10 AM by stephen.
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -64,24 +64,54 @@ class TestMovieBagFacade:
             movie_tags=set(movie_tags_),
         )
 
-    def test_from_find_movie_typed_dict(self):
+    def test_from_find_movie_typed_dict_with_singles(self):
+        title = "Test title"
+        year = "4242"
+        directors = "Test director"
+        duration = "42"
+        notes = "A test note"
+        tags = "Tag 1"
+        movie = config.FindMovieTypedDict(
+            title=title,
+            year=[year],
+            director=directors,
+            minutes=[duration],
+            notes=notes,
+            tags=list(tags),
+        )
+
+        # noinspection PyUnresolvedReferences
+        movie_bag = moviebagfacade.MovieBagFacade.from_find_movie_typed_dict(movie)
+
+        assert movie_bag == MovieBag(
+            title=title,
+            year=MovieInteger(year),
+            directors={directors},
+            duration=MovieInteger(duration),
+            notes=notes,
+            movie_tags=set(tags),
+        )
+
+    def test_from_find_movie_typed_dict_with_doubles(self):
         title = "Test title"
         lo_year = 4242
         hi_year = 4244
         year = [str(lo_year), str(hi_year)]
-        director = "Test director"
+        director_1 = "Michael Metcalf"
+        director_2 = "Nancy Nagulto"
+        directors = director_1 + ", " + director_2
         lo_duration = 42
         hi_duration = 44
         duration = [str(lo_duration), str(hi_duration)]
         notes = "A test note"
-        movie_tags_ = ["Tag 1", "Tag 2"]
+        tags = ["Tag 1", "Tag 2"]
         movie = config.FindMovieTypedDict(
             title=title,
             year=year,
-            director=director,
+            director=directors,
             minutes=duration,
             notes=notes,
-            tags=movie_tags_,
+            tags=tags,
         )
 
         # noinspection PyUnresolvedReferences
@@ -90,8 +120,48 @@ class TestMovieBagFacade:
         assert movie_bag == MovieBag(
             title=title,
             year=MovieInteger(str(lo_year) + "-" + str(hi_year)),
-            directors={director},
+            directors={director_1, director_2},
             duration=MovieInteger(str(lo_duration) + "-" + str(hi_duration)),
             notes=notes,
-            movie_tags=set(movie_tags_),
+            movie_tags=set(tags),
         )
+
+
+def test_convert_to_movie_key():
+    title = "Test Title"
+    year = "4242"
+    movie_bag = MovieBag(title=title, year=MovieInteger(year))
+
+    movie_key = moviebagfacade.convert_to_movie_key(movie_bag)
+
+    assert movie_key == moviebagfacade.MovieKeyTypedDict(title=title, year=int(year))
+
+
+def test_convert_to_movie_update_def():
+    title = "Convert to Update"
+    year = "4242"
+    director_1 = "Harriet House"
+    director_2 = "Ira Indigo"
+    duration = "42"
+    notes = "Movie update definition."
+    tags = {"tag 1", "tag 2", "tag 3"}
+    # noinspection PyTypeChecker
+    movie_bag = MovieBag(
+        title=title,
+        year=MovieInteger(year),
+        duration=MovieInteger(duration),
+        directors=[director_1, director_2],
+        notes=notes,
+        movie_tags=tags,
+    )
+
+    movie = moviebagfacade.convert_to_movie_update_def(movie_bag)
+
+    assert movie == config.MovieUpdateDef(
+        title=title,
+        year=int(year),
+        director=[director_1, director_2],
+        minutes=int(duration),
+        notes=notes,
+        tags=list(tags),
+    )
