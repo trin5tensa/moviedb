02a12390e88acfa83b159f986da07502d30cb274
Stephen Rigden <stepcomsix@iCloud.com>
2025-03-10 15:09:04 -0400

    moviedb-530 moviedb Move `guiwidgets_2`
    Initial set-up for move of guiwidgets_2 MovieGUI, AddMovieGUI, and EditMovieGUI.

---
+++ test/gui_2025/test_movies.py
@@ -0,0 +1,14 @@
+"""Test Module."""
+
+#  Copyright© 2025. Stephen Rigden.
+#  Last modified 3/10/25, 3:09 PM by stephen.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
