d760d7aa60afe5ac4de7102b4042c50a51ebbb3d
Stephen Rigden <stepcomsix@iCloud.com>
2025-03-12 06:54:10 -0400

    moviedb-552 moviedb MovieGUI.__post_init__
    Coded, inspected, and tested. 100% coverage except for stub methods.

--- gui/movies.py
+++ gui/movies.py
@@ -1,7 +1,7 @@
 """This module contains code for movie maintenance."""
 
 #  Copyright© 2025. Stephen Rigden.
-#  Last modified 3/10/25, 2:54 PM by stephen.
+#  Last modified 3/12/25, 6:54 AM by stephen.
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -16,3 +16,62 @@
 # This tkinter import method supports accurate test mocking of tk and ttk.
 import tkinter as tk
 import tkinter.ttk as ttk
+from collections.abc import Callable, Sequence
+from dataclasses import dataclass, KW_ONLY, field
+import queue
+
+from globalconstants import MovieBag
+from gui import common, tk_facade
+
+
+@dataclass
+class MovieGUI:
+    """This base class for movies creates a standard movies input form."""
+
+    parent: tk.Tk
+
+    _: KW_ONLY
+    tmdb_callback: Callable[[str, queue.LifoQueue], None]
+    all_tags: Sequence[str]
+
+    prepopulate: MovieBag = field(default_factory=MovieBag, kw_only=True)
+    # A more convenient data structure for entry fields.
+    entry_fields: dict[
+        str,
+        tk_facade.Entry | tk_facade.Text | tk_facade.Treeview,
+    ] = field(default_factory=dict, init=False, repr=False)
+
+    def __post_init__(self):
+        outer_frame, body_frame, buttonbox = common.create_body_and_buttonbox(
+            self.parent, type(self).__name__.lower(), self.destroy
+        )
+        tmdb_frame = self.create_tmdb_frame(outer_frame)
+        self.fill_body(body_frame)
+        self.populate()
+        self.fill_buttonbox(buttonbox)
+        self.fill_tmdb(tmdb_frame)
+        common.init_button_enablements(self.entry_fields)
+
+    def create_tmdb_frame(self, outer_frame: ttk.Frame) -> ttk.Frame:
+        """Stub method."""
+        pass
+
+    def fill_body(self, body_frame: ttk.Frame):
+        """Stub method."""
+        pass
+
+    def fill_buttonbox(self, buttonbox: ttk.Frame):
+        """Stub method."""
+        pass
+
+    def fill_tmdb(self, tmdb_frame: ttk.Frame):
+        """Stub method."""
+        pass
+
+    def populate(self):
+        """Stub method."""
+        pass
+
+    def destroy(self):
+        """Stub method."""
+        pass
--- test/gui_2025/test_movies.py
+++ test/gui_2025/test_movies.py
@@ -1,7 +1,7 @@
 """Test Module."""
 
 #  Copyright© 2025. Stephen Rigden.
-#  Last modified 3/10/25, 3:09 PM by stephen.
+#  Last modified 3/12/25, 6:54 AM by stephen.
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -12,3 +12,70 @@
 #  GNU General Public License for more details.
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pytest_check import check
+from unittest.mock import MagicMock
+
+from gui import movies
+
+
+class TestMovieGUI:
+    """Tests for the MovieGUI class."""
+
+    def test_post_init(self, tk, ttk, monkeypatch):
+        # Arrange
+        name = movies.MovieGUI.__name__.lower()
+        destroy = MagicMock(name="destroy", autospec=True)
+        monkeypatch.setattr(movies.MovieGUI, "destroy", destroy)
+        create_body_and_buttonbox = MagicMock(
+            name="create_body_and_buttonbox", autospec=True
+        )
+        outer_frame = ttk.Frame()
+        body_frame = ttk.Frame()
+        buttonbox = ttk.Frame()
+        create_body_and_buttonbox.return_value = outer_frame, body_frame, buttonbox
+        monkeypatch.setattr(
+            movies.common, "create_body_and_buttonbox", create_body_and_buttonbox
+        )
+        create_tmdb_frame = MagicMock(name="create_tmdb_frame", autospec=True)
+        tmdb_frame = ttk.Frame
+        create_tmdb_frame.return_value = tmdb_frame
+        monkeypatch.setattr(movies.MovieGUI, "create_tmdb_frame", create_tmdb_frame)
+        fill_body = MagicMock(name="fill_body", autospec=True)
+        monkeypatch.setattr(movies.MovieGUI, "fill_body", fill_body)
+        # noinspection DuplicatedCode
+        populate = MagicMock(name="populate", autospec=True)
+        monkeypatch.setattr(movies.MovieGUI, "populate", populate)
+        fill_buttonbox = MagicMock(name="fill_buttonbox", autospec=True)
+        monkeypatch.setattr(movies.MovieGUI, "fill_buttonbox", fill_buttonbox)
+        fill_tmdb = MagicMock(name="fill_tmdb", autospec=True)
+        monkeypatch.setattr(movies.MovieGUI, "fill_tmdb", fill_tmdb)
+        init_button_enablements = MagicMock(
+            name="init_button_enablements", autospec=True
+        )
+        monkeypatch.setattr(
+            movies.common, "init_button_enablements", init_button_enablements
+        )
+        tmdb_callback = MagicMock(name="tmdb_callback")
+        all_tags = []
+
+        # Act
+        movies_gui = movies.MovieGUI(
+            tk.Tk, tmdb_callback=tmdb_callback, all_tags=all_tags
+        )
+
+        # Assert
+        with check:
+            create_body_and_buttonbox.assert_called_once_with(tk.Tk, name, destroy)
+        with check:
+            create_tmdb_frame.assert_called_once_with(outer_frame)
+        with check:
+            fill_body.assert_called_once_with(body_frame)
+        with check:
+            populate.assert_called_once_with()
+        with check:
+            fill_buttonbox.assert_called_once_with(buttonbox)
+        with check:
+            fill_tmdb.assert_called_once_with(tmdb_frame)
+        with check:
+            init_button_enablements.assert_called_once_with(movies_gui.entry_fields)
